513f380a3fcce48c72f46b24e1b9a26677ef7dfb
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-04-12 16:07:20 +0300

    feat: add retry task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -140,8 +140,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let lastError;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        lastError = error;
+      }
+    }
+    throw lastError;
+  };
 }
 
 /**
